242093bc8099efef0fc1e843273e18916cf48228
Sam arbid <samarbid13@gmail.com>
2022-04-29 22:04:03 +0200

    initiate server

--- server.py
+++ server.py
@@ -1,3 +1,86 @@
-from flask import flask, request, jsonify
+from dataclasses import dataclass
+from sqlite3 import Connection as SQlite3Connection, Cursor, Date
+from datetime import datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
 
-app = flask(__name__)
+# App
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+# SQLite configs
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQlite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foregin_keys=ON;")
+        cursor.close()
+
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+# models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+@app.route("/")
+def hello():
+    return {"message": "Hello!"}
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"],
+        posts=data["posts"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "user created successfully!"}), 200
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_desc():
+    pass
+
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_asc():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_user_by_id(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
