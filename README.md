6595fee3375524ab9c89cfc312529d5fab258637
Samk13 <samarbid13@gmail.com>
2022-05-02 23:45:02 +0200

    playing with templates

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # flask-data-structure
+
+export enviroment variable
+
+```bash
+export FLASK_APP=server.py
+export FLASK_ENV=development
+```
+
+`printenv` -> will show your enviroment variables
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
+from os import abort
 from sqlite3 import Connection as SQlite3Connection, Cursor, Date
 from datetime import datetime
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, jsonify
+import os.path
+from flask import Flask, request, jsonify, render_template, abort
+import json
 
 # App
 app = Flask(__name__)
@@ -18,6 +21,9 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
         cursor.execute("PRAGMA foregin_keys=ON;")
         cursor.close()
 
+
+with open("list_db.json") as json_file:
+    data = json.load(json_file)
 
 db = SQLAlchemy(app)
 now = datetime.now()
@@ -44,7 +50,53 @@ class BlogPost(db.Model):
 
 @app.route("/")
 def hello():
-    return {"message": "Hello!"}
+    return "Welcome!"
+
+
+@app.route("/date")
+def date():
+    return f"date is {datetime.now()}"
+
+
+# add page to show how many times it's been called
+
+
+times = 0
+
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir() + "/static/", filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return src
+    except IOError as exc:
+        return str(exc)
+
+
+@app.route("/times")
+def how_many_times():
+    global times
+    times += 1
+    return render_template("index.html", times=times, x="static/test.jpg")
+
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        global data
+
+        return render_template("card.html", data=data[index], index=index)
+    except IndexError:
+        return render_template("not_found.html")
+        abort(404)
 
 
 @app.route("/user", methods=["POST"])
@@ -82,5 +134,6 @@ def delete_user(user_id):
     pass
 
 
+# Initiate app
 if __name__ == "__main__":
     app.run(debug=True)
